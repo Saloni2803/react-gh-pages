60f48932e71d4332a88f7acd10418f7ffc47dfd4
Saloni2803 <saloniejindal8@gmail.com>
2019-12-09 14:27:54 +0530

    Updates

---
+++ precache-manifest.34435020850c66df9f57a4dfa911c391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "8eeffb4cc3dfa34810f7",
+    "url": "/react-gh-pages/static/js/main.29072f88.chunk.js"
+  },
+  {
+    "revision": "5ad9e9d6fec0ec135cb7",
+    "url": "/react-gh-pages/static/js/2.f6711c6e.chunk.js"
+  },
+  {
+    "revision": "8eeffb4cc3dfa34810f7",
+    "url": "/react-gh-pages/static/css/main.e84f9201.chunk.css"
+  },
+  {
+    "revision": "b1aec3e1b2ea79d1f17876802009713e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
